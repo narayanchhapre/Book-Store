e7df3c4cf3bc10598d07346fdb1c424573939046
narayan chhapre <narayanchhapre2001@gmail.com>
2024-03-02 11:22:21 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConfig.js");
+const booksRouter = require("./routes/books.js");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cors(
+    {
+        origin: "https://sahilatahar-book-store.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
+
+connectDB();
+
+app.use("/books", booksRouter);
+
+app.use("/", (req, res) => {
+    res.send("Hello, Server is running")
+});
+
+mongoose.connection.once("open", () => {
+    app.listen(PORT);
+});
